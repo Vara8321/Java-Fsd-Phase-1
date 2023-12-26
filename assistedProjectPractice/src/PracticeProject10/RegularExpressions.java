5b0a2374cbec47017a0351ffa9f5fee7ce1b5c44
varaprasad <chitteti.varaprasad@gmail.com>
2023-12-26 10:30:40 +0530

    commits

--- assistedProjectPractice/src/PracticeProject10/RegularExpressions.java
+++ assistedProjectPractice/src/PracticeProject10/RegularExpressions.java
@@ -7,6 +7,16 @@ public class RegularExpressions {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Pattern ptrn=Pattern.compile("Hello21",Pattern.CASE_INSENSITIVE);
+		Matcher matcher=ptrn.matcher("Hello21 world");
+		boolean matchFound=matcher.find();
+		if(matchFound){
+			System.out.println("Match found");
+		}else{
+			System.out.println("Match Not found");
+		}
+
+
 		String pattern = "[a-z]+";
 		String check = "Regular Expressions";
 		Pattern p = Pattern.compile(pattern);
