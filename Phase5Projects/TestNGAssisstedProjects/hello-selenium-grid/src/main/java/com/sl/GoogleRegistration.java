4604370bc279b32a2cd550f8e5bbe696f40160d4
varaprasad <chitteti.varaprasad@gmail.com>
2024-03-07 14:23:22 +0530

    commited

---
+++ Phase5Projects/TestNGAssisstedProjects/hello-selenium-grid/src/main/java/com/sl/GoogleRegistration.java
@@ -0,0 +1,83 @@
+package com.sl;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class GoogleRegistration {
+	
+	public WebDriver driver;
+	SoftAssert softAssert = new SoftAssert();
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.printf("Inside %s and thread-id is %s \n", "beforeSuite", Thread.currentThread().getId());
+		
+		//Connect to hub and execute the test cases in this class.
+		
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setPlatform(Platform.WIN11);
+		caps.setBrowserName("chrome");
+		
+		
+		try {
+			driver = new RemoteWebDriver(new URL("http://192.168.0.144:4444/wd/hub"), caps);
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.printf("Inside %s and thread-id is %s \n", "afterSuite", Thread.currentThread().getId());
+
+		driver.close();
+	}
+	
+	@Test(dataProvider = "googleNewUserData")
+	public void googleNewAccCreation(String firstName, String lastName) {
+		System.out.printf("Inside GoogleRegistration:TESTCASE %s and thread-id is %s \n", "googleNewAccCreation", Thread.currentThread().getId());
+		String baseUrl = "https://accounts.google.com/signup/v2/createaccount?flowEntry=SignUp";
+		driver.get(baseUrl);
+
+		// Locate the first name feild using id
+		WebElement firstNameTF = driver.findElement(By.id("firstName"));
+		
+		softAssert.assertNotNull(firstNameTF);
+
+		firstNameTF.sendKeys(firstName);
+
+		// Locate the last name feild using name
+		WebElement lastNameTF = driver.findElement(By.name("lastName"));
+		
+		softAssert.assertNotNull(lastNameTF);
+		
+		lastNameTF.sendKeys(lastName);
+		
+	}
+	
+	@DataProvider(name = "googleNewUserData")
+	public Object[][] googleNewUserData() {
+		return new Object[][] {
+
+				{ "Krishna", "Varma" },
+				{ "Ravi", "Sarma" },
+				{ "Shalini", "Yadav" }
+				
+			};
+	}
+	
+
+}
---
+++ Phase5Projects/TestNGAssisstedProjects/hello-selenium-testng/src/main/java/com/sl/GoogleSearch.java
@@ -0,0 +1,115 @@
+package com.sl;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class GoogleSearch {
+
+	 public WebDriver driver = new FirefoxDriver();
+
+	static {
+		// For Chrome Driver only
+		// Downlaod from
+		// https://storage.googleapis.com/chrome-for-testing-public/118.0.5962.0/win64/chromedriver-win64.zip
+		// and upzip
+		
+		String chromeDriverLocation = "C:\\Users\\chitt\\Downloads\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe";
+		System.setProperty("webdriver.chrome.driver", chromeDriverLocation);
+		
+	};
+
+	//WebDriver driver = new ChromeDriver();
+	//WebDriver driver = null;
+
+	SoftAssert softAssert = new SoftAssert();
+
+	@BeforeSuite
+	public void myBeforeSuite() {
+		System.out.println("INSIDE myBeforeSuite");			
+	}
+	
+	@AfterSuite
+	public void myAfterSuite() {
+		System.out.println("INSIDE myAfterSuite");		
+	}
+	
+	@BeforeTest
+	public void myBeforeTest() {
+		System.out.println("INSIDE myBeforeTest");			
+	}
+	
+	@AfterTest
+	public void myAfterTest() {
+		System.out.println("INSIDE myAfterTest");		
+	}
+	
+	@BeforeClass
+	public void myBeforeClass() {
+		System.out.println("INSIDE myBeforeClass");
+		driver = new FirefoxDriver();		
+	}
+	
+	@AfterClass
+	public void myAfterClass() {
+		System.out.println("INSIDE myAfterClass");
+		driver.close();
+	}
+	
+	
+	@BeforeMethod
+	public void myBeforeMethod() {
+		System.out.println("INSIDE myBeforeMethod");
+		//driver = new FirefoxDriver();		
+	}
+	
+	@AfterMethod
+	public void myAfterMethod() {
+		System.out.println("INSIDE myAfterMethod");
+		//driver.close();
+	}
+		
+	
+	@Test
+	public void searchGoogleAndTestTitleText() {
+		
+		System.out.printf("Inside TESTCASE %s and thread-id is %s \n", "searchGoogleAndTestTitleText", Thread.currentThread().getId());
+
+		driver.get("http://www.google.com");
+
+		System.out.println("Title of google page is " + driver.getTitle());
+
+		softAssert.assertEquals(driver.getTitle(), "Google");
+
+	}
+	
+	// Challenge: Check of Google search text feild appears and contains text "flowers"
+	@Test
+	public void testGoogleSearchTextFeildContainsFlowers() {
+		System.out.printf("Inside TESTCASE %s and thread-id is %s \n", "testGoogleSearchTextFeildContainsFlowers", Thread.currentThread().getId());
+
+		driver.get("http://www.google.com?q=flowers");	
+		
+		WebElement searchTFElement = driver.findElement(By.name("q"));
+		
+		String searchTFText = searchTFElement.getAttribute("value");
+		
+		System.out.println("Search TF Text="+searchTFText);
+
+		softAssert.assertEquals(searchTFText, "flowers"); // Actual Result , Expected Result
+
+	}
+
+}
